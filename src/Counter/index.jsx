2bae6effc72b4124f271be2c1aae84ef85d3d776
Baxtiyor0101 <oqonboyevb@gmail.com>
2022-12-15 11:51:00 +0500

    test

--- src/Counter/index.jsx
+++ src/Counter/index.jsx
@@ -1,6 +1,7 @@
 import React, { useReducer, useState } from "react";
 function Counter() {
   const [count, setCount] = useState(0);
+  const [option, setOption] = useState(1);
   const reducer = (state, action) => {
     console.log(state, action.payload);
     switch (action.type) {
@@ -14,7 +15,12 @@ function Counter() {
         return state;
     }
   };
+
   const [counter, dispatch] = useReducer(reducer, 0);
+  const onSelect = (e) => {
+    console.log(typeof +e.target.value);
+    setOption(+e.target.value);
+  };
   return (
     <div>
       <h1>Counter {count}</h1>
@@ -24,8 +30,16 @@ function Counter() {
       <h1>Counter with reducer {counter}</h1>
       <button onClick={() => dispatch({ type: "plus" })}>+</button>
       <button onClick={() => dispatch({ type: "minus" })}>-</button>
-      <button onClick={() => dispatch({ type: "amaunt", payload: 5 })}>
-        5
+      <br />
+      <select onChange={onSelect} defaultValue={1} name="" id="">
+        <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option>
+        <option value={4}>4</option>
+        <option value={5}>5</option>
+      </select>
+      <button onClick={() => dispatch({ type: "amaunt", payload: option })}>
+        {option}
       </button>
     </div>
   );
